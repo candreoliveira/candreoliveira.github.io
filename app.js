ae314f0c7beb3438262b5d20ccf6268d5f807e4a
Carlos Oliveira <candreoliveira@gmail.com>
2014-07-26 11:53:04 -0300

    Try ph page

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 'use strict';
-
-angular.module('app', [])
-  .config(function ($routeProvider) {
+angular.module('app', ['ngMask', 'ngRoute'])
+  .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/ngMask.html'
       })
       .when('/ngMask', {
-        template: '<iframe src=\'https://raw.githubusercontent.com/candreoliveira/ngMask/master/index.html\'>'
+        template: ''
       })
       .otherwise({
         redirectTo: '/'
       });
-  });
+  }]);
